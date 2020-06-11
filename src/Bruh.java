55505aa4119cfe8ded4e4ee8ae4566aeb11aa713
xf8b <xf8b.inquiries@protonmail.com>
2020-06-11 16:46:33 -0700

    Stuff that I can't remember

--- src/Bruh.java
+++ src/Bruh.java
@@ -2,16 +2,25 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Bruh {
     public static void main(String[] args) throws IOException {
         String bruhs = new String(Files.readAllBytes(Paths.get("src/bruhs.txt")));
-        BigInteger bigInteger = new BigInteger(bruhs);
-        if (args[0].equals("-integerAdditionAmount")) {
-            for (int i = 0; i < Integer.parseInt(args[1]); i++) {
-                bigInteger = bigInteger.add(bigInteger);
+        BigInteger bruhAmount = new BigInteger(bruhs);
+        BigInteger stopAt = bruhAmount;
+        if (!(args.length == 0)) {
+            if (args[0].equals("-integerAdditionAmount")) {
+                for (int i = 0; i < Integer.parseInt(args[1]); i++) {
+                    bruhAmount = bruhAmount.add(bruhAmount);
+                }
+            }
+            if (args[0].equals("-stopAt")) {
+                stopAt = new BigInteger(args[1]);
             }
         }
-        System.out.println(bigInteger);
+        for (BigInteger i = BigInteger.valueOf(0); i.compareTo(stopAt) < 0; i = i.add(BigInteger.valueOf(1))) {
+            System.out.print("bruh ");
+        }
     }
 }
